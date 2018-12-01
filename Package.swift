c62221478ebd93b20548f51d5e1b75cf84bddfdb
Alexander Alemayhu <alexander@alemayhu.com>
2018-12-01 17:33:09 +0100

    Remove all the expression related bits
    We will import it as a seperate library.

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "cal",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(path: "../Expression")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
