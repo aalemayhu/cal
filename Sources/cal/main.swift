1fa47d672a7d2a7220c6cb1a8911ee74053b1764
Alexander Alemayhu <alexander@alemayhu.com>
2018-12-01 19:29:02 +0100

    Use the expression library
    o Remove outdated test

--- Sources/cal/main.swift
+++ Sources/cal/main.swift
@@ -1,1 +1,10 @@
-print("Hello, world!")
+import Expression
+import Foundation
+
+guard CommandLine.argc > 1 else {
+    print("Error: please supply a expression like ./cal 2+2")
+    exit(1)
+}
+
+let e = Expression(value: CommandLine.arguments.dropFirst().joined())
+print(e.evaluate())
--- Tests/calTests/calTests.swift
+++ Tests/calTests/calTests.swift
@@ -3,38 +3,7 @@ import class Foundation.Bundle
 
 final class calTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-
-        // Some of the APIs that we use below are available in macOS 10.13 and above.
-        guard #available(macOS 10.13, *) else {
-            return
-        }
-
-        let fooBinary = productsDirectory.appendingPathComponent("cal")
-
-        let process = Process()
-        process.executableURL = fooBinary
-
-        let pipe = Pipe()
-        process.standardOutput = pipe
-
-        try process.run()
-        process.waitUntilExit()
-
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(data: data, encoding: .utf8)
-
-        XCTAssertEqual(output, "Hello, world!\n")
-    }
-
-    func testSimpleAddition() throws {
-        let input = "2 + 2"
-        let expected = "4"
-
-        let actual = Expression(input)
-        XCTAssertEqual(expected, actual.evaluate())
+        print("TODO: add some tests")
     }
 
     /// Returns path to the built products directory.
@@ -51,6 +20,5 @@ final class calTests: XCTestCase {
 
     static var allTests = [
         ("testExample", testExample),
-        ("testSimpleAddition", testSimpleAddition),
     ]
 }
